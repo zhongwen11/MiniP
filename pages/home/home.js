9a8a518eba5ca6324dc403acac2d42f71be69e0b
aliwen <aliwen@qq.com>
2019-11-07 23:31:40 +0800

    good ninght

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: 'Aliwen小栈',
+    age: 18,
+    students:[
+      {id:1,name:'aaa',age:12},
+      {id:2,name:'bbb',age:14},
+      {id:3,name:'ccc',age:16}
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBtnCut(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })

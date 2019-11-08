af569303caaeebf6708b3b564f4b6b3547a021d1
aliwen <aliwen@qq.com>
2019-11-08 15:32:16 +0800

    注册小程序页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    list:[],
     name: 'Aliwen小栈',
     age: 18,
     students:[
@@ -22,6 +23,23 @@ Page({
   handleBtnCut(){
     this.setData({
       counter: this.data.counter - 1
+    })
+  },
+  handleGetUserInfo(event){
+    console.log(event)
+  },
+  // 页面发送网络请求
+  onLoad(){
+    console.log('onLoad')
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      success: (res)=>{
+        console.log(res)
+        const data = res.data.data.list;
+        this.setData({
+          list: data
+        })
+      }
     })
   }
 })
